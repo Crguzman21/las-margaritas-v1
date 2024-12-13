82e93ae43ad54d0bb3cbb960b8035ebd008731d5
cristofer guzman <tito.2101@hotmail.com>
2024-12-12 19:59:06 -0500

    inicio

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,19 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import './App.css'; 
+
 
 function App() {
-  const [count, setCount] = useState(0)
+  const nameStore = "Salsamentaria Las Margaritas";
 
+  function saludarUsuario(nameUser) {
+    const saludo = `Bienvenido ${nameUser} a ${nameStore} tu tienda de confianza`;
+    return saludo;
+  }
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
+
+      <h1> {nameStore} </h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+        <p>{saludarUsuario("Horacio")}</p>
       </div>
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
